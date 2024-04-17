0e93bcfc21677aa17fb2a032c9c082397ccecd8c
mayurrohokale <mayurrohokale12345@gmail.com>
2024-04-17 23:42:27 +0530

    fixed some errors

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,8 @@
-import React from "react";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 
 
 const Dashboard = ()=> {
 
-    const session  = getServerSession(); // Get the
-    if(!session){
-        redirect("/");
-    }
+  
     return (
      <div className="text-[25px] flex text-center  ">welcome to dashboard</div>
     );
